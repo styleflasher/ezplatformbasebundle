fcbdac23adbd915b84d8fb391bbcbf1d2e9c5162
Andreas Adelsberger <andreas.adelsberger@styleflasher.at>
2017-02-02 15:24:44 +0100

    Added search controller.

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Styleflasher\eZPlatformBaseBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use Pagerfanta\Pagerfanta;
+use eZ\Publish\Core\Pagination\Pagerfanta\ContentSearchAdapter;
+use eZ\Publish\Core\Pagination\Pagerfanta\LocationSearchAdapter;
+use Symfony\Component\HttpFoundation\Request;
+
+class SearchController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        $params['results'] = [];
+        $repository = $this->container->get('ezpublish.api.repository');
+        $queryString= $request->query->get('q');
+        $params['q'] = $queryString;
+        if ($queryString) {
+            $searchService = $repository->getSearchService();
+            $query = new Query();
+            $query->filter = new Criterion\LogicalAnd(
+                array(
+                    new Criterion\FullText($queryString),
+                    new Criterion\Visibility(Criterion\Visibility::VISIBLE),
+                    new Criterion\LanguageCode(array('ger-DE'), true),
+                )
+            );
+
+            $pager = new Pagerfanta(
+                new ContentSearchAdapter($query, $searchService)
+            );
+            $limit = 10;
+            $pager->setMaxPerPage($limit);
+            $pager->setCurrentPage($request->get('page', 1));
+            $params['results'] = $pager;
+        }
+
+        return $this->render('StyleflashereZPlatformBaseBundle:search:search.html.twig', $params);
+    }
+}
