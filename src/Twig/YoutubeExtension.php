e6d6eb14bb984a9f6a707709c982f5ce543fba41
Andreas Adelsberger <andreas.adelsberger@styleflasher.at>
2017-03-17 15:03:50 +0100

    Added twig filter to get the youtube id

--- src/Twig/YoutubeExtension.php
+++ src/Twig/YoutubeExtension.php
@@ -7,6 +7,7 @@ class YoutubeExtension extends \Twig_Extension
     {
         return array(
             new \Twig_SimpleFilter('youtube', array($this, 'youtubeFilter'), array('is_safe'=>array('all'))),
+            new \Twig_SimpleFilter('youtubeid', array($this, 'youtubeIdFilter'), array('is_safe'=>array('all'))),
         );
     }
 
@@ -16,6 +17,25 @@ class YoutubeExtension extends \Twig_Extension
         $youtubeId = trim($my_array_of_vars['v']);
         return "https://youtube.com/embed/".$youtubeId."?autoplay=".
             $autoplay."&controls=".$controls."&showinfo=".$showinfo."&autohide=".$autohide;
+    }
+
+    public function youtubeIdFilter($url)
+    {
+        $parts = parse_url($url);
+        if (isset($parts['query'])) {
+            parse_str($parts['query'], $qs);
+            if (isset($qs['v'])) {
+                return $qs['v'];
+            }
+            if (isset($qs['vi'])) {
+                return $qs['vi'];
+            }
+        }
+        if (isset($parts['path'])) {
+            $path = explode('/', trim($parts['path'], '/'));
+            return $path[count($path)-1];
+        }
+        return false;
     }
 
     public function getName()
