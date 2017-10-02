334807994bd9c87004ab53b03424b80d68527919
Andreas Adelsberger <andreas.adelsberger@styleflasher.at>
2017-10-02 15:57:28 +0200

    added default template for search view

--- src/DependencyInjection/StyleflashereZPlatformBaseExtension.php
+++ src/DependencyInjection/StyleflashereZPlatformBaseExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\Yaml\Yaml;
 use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\SiteAccessAware\ConfigurationProcessor;
 
 /**
- * This is the class that loads and manages your bundle configuration
+ * This is the class that loads and manages your bundle configuration.
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
@@ -20,50 +20,135 @@ class StyleflashereZPlatformBaseExtension extends Extension implements PrependEx
 {
     /**
      * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
-        
-        $processor = new ConfigurationProcessor( $container, 'styleflashere_z_platform_base' );
+        $loader->load('parameters.yml');
+        $loader->load('system_settings.yml');
+
+        $processor = new ConfigurationProcessor($container, 'styleflashere_z_platform_base');
         $processor->mapConfig(
             $config,
-            function ( $scopeSettings, $currentScope, $contextualizer ) {
-                $contextualizer->setContextualParameter( 'search.searchresult_view', $currentScope, $scopeSettings['search']['searchresult_view'] );
+            function ($scopeSettings, $currentScope, $contextualizer) {
+                $contextualizer->setContextualParameter(
+                    'search.searchresult_view',
+                    $currentScope,
+                    $scopeSettings['search']['searchresult_view']
+                );
 
-                $contextualizer->setContextualParameter( 'sujets.fallback_container_location_id', $currentScope, $scopeSettings['sujets']['fallback_container_location_id'] );
-                $contextualizer->setContextualParameter( 'sujets.sujetclasses', $currentScope, $scopeSettings['sujets']['sujetclasses'] );
+                $contextualizer->setContextualParameter(
+                    'sujets.fallback_container_location_id',
+                    $currentScope,
+                    $scopeSettings['sujets']['fallback_container_location_id']
+                );
+                $contextualizer->setContextualParameter(
+                    'sujets.sujetclasses',
+                    $currentScope,
+                    $scopeSettings['sujets']['sujetclasses']
+                );
 
-                $contextualizer->setContextualParameter( 'menu.has_submenu', $currentScope, $scopeSettings['menu']['has_submenu'] );
-                $contextualizer->setContextualParameter( 'menu.levels', $currentScope, $scopeSettings['menu']['levels'] );
-                $contextualizer->setContextualParameter( 'menu.main.classes', $currentScope, $scopeSettings['menu']['main']['classes'] );
-                $contextualizer->setContextualParameter( 'menu.main.excluded_location_ids', $currentScope, $scopeSettings['menu']['main']['excluded_location_ids'] );
-                $contextualizer->setContextualParameter( 'menu.sub.classes', $currentScope, $scopeSettings['menu']['sub']['classes'] );
-                $contextualizer->setContextualParameter( 'menu.sub.excluded_location_ids', $currentScope, $scopeSettings['menu']['sub']['excluded_location_ids'] );
+                $contextualizer->setContextualParameter(
+                    'menu.has_submenu',
+                    $currentScope,
+                    $scopeSettings['menu']['has_submenu']
+                );
+                $contextualizer->setContextualParameter(
+                    'menu.levels',
+                    $currentScope,
+                    $scopeSettings['menu']['levels']
+                );
+                $contextualizer->setContextualParameter(
+                    'menu.main.classes',
+                    $currentScope,
+                    $scopeSettings['menu']['main']['classes']
+                );
+                $contextualizer->setContextualParameter(
+                    'menu.main.excluded_location_ids',
+                    $currentScope,
+                    $scopeSettings['menu']['main']['excluded_location_ids']
+                );
+                $contextualizer->setContextualParameter(
+                    'menu.sub.classes',
+                    $currentScope,
+                    $scopeSettings['menu']['sub']['classes']
+                );
+                $contextualizer->setContextualParameter(
+                    'menu.sub.excluded_location_ids',
+                    $currentScope,
+                    $scopeSettings['menu']['sub']['excluded_location_ids']
+                );
 
-                $contextualizer->setContextualParameter( 'right_column.social_media.show', $currentScope, $scopeSettings['right_column']['social_media']['show'] );
-                $contextualizer->setContextualParameter( 'right_column.social_media.facebook', $currentScope, $scopeSettings['right_column']['social_media']['facebook'] );
-                $contextualizer->setContextualParameter( 'right_column.social_media.twitter', $currentScope, $scopeSettings['right_column']['social_media']['twitter'] );
-                $contextualizer->setContextualParameter( 'right_column.social_media.linkedin', $currentScope, $scopeSettings['right_column']['social_media']['linkedin'] );
-                $contextualizer->setContextualParameter( 'right_column.widgets.show', $currentScope, $scopeSettings['right_column']['widgets']['show'] );
-                $contextualizer->setContextualParameter( 'right_column.widgets.classes', $currentScope, $scopeSettings['right_column']['widgets']['classes'] );
-                $contextualizer->setContextualParameter( 'right_column.additional_menu_level.show', $currentScope, $scopeSettings['right_column']['additional_menu_level']['show'] );
-                $contextualizer->setContextualParameter( 'right_column.additional_menu_level.depth', $currentScope, $scopeSettings['right_column']['additional_menu_level']['depth'] );
-                $contextualizer->setContextualParameter( 'right_column.additional_menu_level.classes', $currentScope, $scopeSettings['right_column']['additional_menu_level']['classes'] );
-                $contextualizer->setContextualParameter( 'right_column.additional_menu_level.excluded_location_ids', $currentScope, $scopeSettings['right_column']['additional_menu_level']['excluded_location_ids'] );
+                $contextualizer->setContextualParameter(
+                    'right_column.social_media.show',
+                    $currentScope,
+                    $scopeSettings['right_column']['social_media']['show']
+                );
+                $contextualizer->setContextualParameter(
+                    'right_column.social_media.facebook',
+                    $currentScope,
+                    $scopeSettings['right_column']['social_media']['facebook']
+                );
+                $contextualizer->setContextualParameter(
+                    'right_column.social_media.twitter',
+                    $currentScope,
+                    $scopeSettings['right_column']['social_media']['twitter']
+                );
+                $contextualizer->setContextualParameter(
+                    'right_column.social_media.linkedin',
+                    $currentScope,
+                    $scopeSettings['right_column']['social_media']['linkedin']
+                );
+                $contextualizer->setContextualParameter(
+                    'right_column.widgets.show',
+                    $currentScope,
+                    $scopeSettings['right_column']['widgets']['show']
+                );
+                $contextualizer->setContextualParameter(
+                    'right_column.widgets.classes',
+                    $currentScope,
+                    $scopeSettings['right_column']['widgets']['classes']
+                );
+                $contextualizer->setContextualParameter(
+                    'right_column.additional_menu_level.show',
+                    $currentScope,
+                    $scopeSettings['right_column']['additional_menu_level']['show']
+                );
+                $contextualizer->setContextualParameter(
+                    'right_column.additional_menu_level.depth',
+                    $currentScope,
+                    $scopeSettings['right_column']['additional_menu_level']['depth']
+                );
+                $contextualizer->setContextualParameter(
+                    'right_column.additional_menu_level.classes',
+                    $currentScope,
+                    $scopeSettings['right_column']['additional_menu_level']['classes']
+                );
+                $contextualizer->setContextualParameter(
+                    'right_column.additional_menu_level.excluded_location_ids',
+                    $currentScope,
+                    $scopeSettings['right_column']['additional_menu_level']['excluded_location_ids']
+                );
             }
         );
     }
-    
-    public function prepend( ContainerBuilder $container )
+
+    /**
+     * prepend
+     *
+     * @param ContainerBuilder $container
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    public function prepend(ContainerBuilder $container)
     {
         $stfConfigFile = __DIR__ . '/../Resources/config/default_settings.yml';
-        $stfConfig = Yaml::parse( file_get_contents( $stfConfigFile ) );
-        $container->prependExtensionConfig( 'styleflashere_z_platform_base', $stfConfig );
-        $container->addResource( new FileResource( $stfConfigFile ) );
+        $stfConfig = Yaml::parse(file_get_contents($stfConfigFile));
+        $container->prependExtensionConfig('styleflashere_z_platform_base', $stfConfig);
+        $container->addResource(new FileResource($stfConfigFile));
     }
 }
